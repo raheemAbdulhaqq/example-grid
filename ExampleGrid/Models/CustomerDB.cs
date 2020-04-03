938d601b551e0b7b99bb256aaf487556d3b3b78b
Raheem Abdulhaqq <raheemabdulhaqq2020@gmail.com>
2020-04-04 00:49:04 +0100

    testing datatables

---
+++ ExampleGrid/Models/CustomerDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleGrid.Models
+{
+    public class CustomerDB
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string Phoneno { get; set; }
+    }
+}
---
+++ ExampleGrid/Models/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleGrid.Models
+{
+    public class DatabaseOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<CustomerDB> CustomerDB { get; set; }
+    }
+}
+
